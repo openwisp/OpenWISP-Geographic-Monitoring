7348228435149fcba910cf750e90345946f0a3ce
Matteo Latini <mtylty@gmail.com>
2011-03-06 18:06:36 +0000

    refactors wisps index view

--- app/models/wisp.rb
+++ app/models/wisp.rb
@@ -2,4 +2,16 @@ class Wisp < ActiveRecord::Base
   acts_as_authorization_object
 
   has_many :hotspots
+
+  def hotspots_up
+    hotspots.select{|hs| hs.up?}
+  end
+
+  def hotspots_down
+    hotspots.select{|hs| hs.down?}
+  end
+
+  def hotspots_unknown
+    hotspots.select{|hs| hs.unknown?}
+  end
 end
