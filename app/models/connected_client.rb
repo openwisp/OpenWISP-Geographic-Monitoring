69e56d7726c7fb009d019bfb11516534a2684d18
Matteo Latini <mtylty@gmail.com>
2011-05-12 10:24:44 +0000

    fixes little l10n byg

--- app/models/connected_client.rb
+++ app/models/connected_client.rb
@@ -6,10 +6,6 @@ class ConnectedClient
     @mac_address = client['mac_address']
   end
 
-  def last_activity_short
-    last_activity.to_s(:short)
-  end
-
   def vendor
     macvendor = MacVendor.find_by_mac_address(mac_address)
     macvendor.nil? ? I18n.t(:Unknown) : macvendor.vendor
