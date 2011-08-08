192c96e6ab83ed5d7a360fbd093ea8985d633e39
Matteo Latini <mtylty@gmail.com>
2011-08-08 11:36:38 +0000

    fixes mispelled has_many

--- app/models/access_point.rb
+++ app/models/access_point.rb
@@ -11,7 +11,7 @@ class AccessPoint < ActiveRecord::Base
   has_one :property_set, :autosave => true, :dependent => :destroy
   has_many :activities
   has_many :activity_histories
-  has_many :associated_user_count
+  has_many :associated_user_counts
   has_many :associated_user_count_histories
 
   delegate :reachable, :to => :property_set, :allow_nil => true
