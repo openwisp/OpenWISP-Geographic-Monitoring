4cf63ed2503613a87753628478dea5dc28f7ebc1
Matteo Latini <mtylty@gmail.com>
2011-08-08 12:50:18 +0000

    adds needed model logic

--- app/models/associated_user_count.rb
+++ app/models/associated_user_count.rb
@@ -1,3 +1,11 @@
 class AssociatedUserCount < ActiveRecord::Base
   belongs_to :access_point
+
+  default_scope order(:start_time)
+
+  def as_json(options={})
+    # Time should be in unix epoch time in
+    # milliseconds...
+    [ created_at.to_i * 1000, count ]
+  end
 end
--- app/models/associated_user_count_history.rb
+++ app/models/associated_user_count_history.rb
@@ -1,7 +1,19 @@
 class AssociatedUserCountHistory < ActiveRecord::Base
   belongs_to :access_point
 
+  default_scope order(:start_time)
+
+  def as_json(options={})
+    # Time should be in unix epoch time in
+    # milliseconds...
+    [ start_time.to_i * 1000, count ]
+  end
+
   def self.older_than(time)
     where(:start_time => time.to_time..Time.now)
+  end
+
+  def self.observe(from, to)
+    where(:last_time => from..to)
   end
 end
