5e3f38571680574ac7e4138b93b3afdbe65b01d6
Matteo Latini <mtylty@gmail.com>
2011-09-16 10:17:57 +0000

    adds proper licenses

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,3 +1,20 @@
+# This file is part of the OpenWISP Geographic Monitoring
+#
+# Copyright (C) 2010 CASPUR (wifi@caspur.it)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class Cluster
   def initialize(access_points)
     if access_points.is_a?(Array) && access_points.all? {|access_point| access_point.is_a?(AccessPoint)}
