b74de412e44869aabcff81921fd4bd3b0bb3c44e
Davide Guerri <davide.guerri@gmail.com>
2011-09-21 07:48:49 +0000

    adds multi-threading and synchronization to users count monitoring and consolidation functions

--- lib/workers/monitoring_worker.rb
+++ lib/workers/monitoring_worker.rb
@@ -25,7 +25,8 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
   MAX_THREADS = 10
   PING_TIMEOUT = 5
 
-  @@semaphore = Mutex.new
+  @@monitoring_semaphore = Mutex.new
+  @@users_count_semaphore = Mutex.new
 
   def create(args = nil)
     # this method is called, when worker is loaded for the first time
@@ -36,14 +37,9 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
     threads = []
     AccessPoint.all.each do |ap|
 
+      # spawn a new thread if there is a "slot" for it. Otherwise, wait for an empty slot
       while threads.length >= MAX_THREADS
-        threads.delete_if do |th|
-          if !th.alive?
-            th.join()
-          else
-            false
-          end
-        end
+        threads.delete_if { |th| th.alive? ? false : th.join() }
         sleep(0.2)
       end
 
@@ -51,12 +47,13 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
         begin
           pt = Net::Ping::External.new(ap.ip, nil, PING_TIMEOUT)
           reachable = pt.ping?
-          act = ap.activities.build( :status => reachable ) if ap.known? || (ap.unknown? && reachable)
+          act = ap.activities.build(:status => reachable) if ap.known? || (ap.unknown? && reachable)
         rescue
-          act = ap.activities.build( :status => false ) if ap.known?
+          act = ap.activities.build(:status => false) if ap.known?
         end
         if act
-          @@semaphore.synchronize {
+          # avoid race conditions with the consolidate_access_points_monitoring() function
+          @@monitoring_semaphore.synchronize {
             act.save!
             act.status ? ap.reachable! : ap.unreachable!
           }
@@ -65,14 +62,9 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
 
     end
 
-    while threads.length > 0
-      threads.delete_if do |th|
-        if !th.alive?
-          th.join()
-        else
-          false
-        end
-      end
+    # collect remaining threads that are finished theirs job
+    while threads.length >= MAX_THREADS
+      threads.delete_if { |th| th.alive? ? false : th.join() }
       sleep(0.2)
     end
 
@@ -81,7 +73,8 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
   def consolidate_access_points_monitoring
     AccessPoint.all.each do |ap|
       begin
-        @@semaphore.synchronize {
+        # avoid race conditions with the access_points_monitoring() function
+        @@monitoring_semaphore.synchronize {
           if ap.activities.count > 0
             last_history_time = ap.activity_histories.last.try(:last_time)
 
@@ -109,41 +102,64 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
   end
 
   def associated_user_counts_monitoring
+    threads = []
+
     Wisp.all.each do |wisp|
-      begin
-        if wisp.owmw_enabled?
-          aps_with_users = []
-          AssociatedUser.active_resource_from(wisp.owmw_url, wisp.owmw_username, wisp.owmw_password)
 
-          AssociatedUser.all.group_by(&:access_point_id).each do |ap_id, users|
-            begin
-              AssociatedUserCount.create!(:count => users.count, :access_point_id => ap_id)
-              aps_with_users << ap_id
-            rescue Exception => e
-              puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}', access point id '#{ap_id}': #{e}"
-              next
-            end
-          end
-
-          if aps_with_users.empty?
-            aps_without_users = wisp.access_points
-          else
-            aps_without_users = wisp.access_points.where(["id NOT IN (?)", aps_with_users])
-          end
-
-          aps_without_users.each do |ap|
-            begin
-              AssociatedUserCount.create!(:count => 0, :access_point_id => ap.id)
-            rescue Exception => e
-              puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}', access point '#{ap.hostname}': #{e}"
-              next
-            end
-          end
-        end
-      rescue Exception => e
-        puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}': #{e}"
-        next
+      # spawn a new thread if there is a "slot" for it. Otherwise, wait for an empty slot
+      while threads.length >= MAX_THREADS
+        threads.delete_if { |th| th.alive? ? false : th.join() }
+        sleep(0.2)
       end
+
+      if wisp.owmw_enabled?
+        threads.push(Thread.new do
+          begin
+            aps_with_users = []
+            AssociatedUser.active_resource_from(wisp.owmw_url, wisp.owmw_username, wisp.owmw_password)
+
+            AssociatedUser.all.group_by(&:access_point_id).each do |ap_id, users|
+              begin
+                # avoid race conditions with the consolidate_associated_user_counts_monitoring() function
+                @@users_count_semaphore.synchronize {
+                  AssociatedUserCount.create!(:count => users.count, :access_point_id => ap_id)
+                }
+                aps_with_users << ap_id
+              rescue Exception => e
+                puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}', access point id '#{ap_id}': #{e}"
+                next
+              end
+            end
+
+            if aps_with_users.empty?
+              aps_without_users = wisp.access_points
+            else
+              aps_without_users = wisp.access_points.where(["id NOT IN (?)", aps_with_users])
+            end
+
+            aps_without_users.each do |ap|
+              begin
+                # avoid race conditions with the consolidate_associated_user_counts_monitoring() function
+                @@users_count_semaphore.synchronize {
+                  AssociatedUserCount.create!(:count => 0, :access_point_id => ap.id)
+                }
+              rescue Exception => e
+                puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}', access point '#{ap.hostname}': #{e}"
+                next
+              end
+            end
+          rescue Exception => e
+            puts "[#{Time.now}] Problem in associated_user_counts_monitoring() for wisp '#{wisp.name}': #{e}"
+          end
+        end)
+      end
+
+    end
+
+    # collect remaining threads that are finished theirs job
+    while threads.length >= MAX_THREADS
+      threads.delete_if { |th| th.alive? ? false : th.join() }
+      sleep(0.2)
     end
   end
 
@@ -155,24 +171,27 @@ class MonitoringWorker < BackgrounDRb::MetaWorker
 
           wisp.access_points.each do |ap|
             begin
-              if ap.associated_user_counts.count > 0
-                last_history_time = ap.associated_user_count_histories.last.try(:last_time)
+              # avoid race conditions with the associated_user_counts_monitoring() function
+              @@users_count_semaphore.synchronize {
+                if ap.associated_user_counts.count > 0
+                  last_history_time = ap.associated_user_count_histories.last.try(:last_time)
 
-                if last_history_time
-                  first_time = ap.associated_user_counts.where('created_at > ?', last_history_time).first.created_at.change(:min => 0, :sec => 0)
-                else
-                  first_time = ap.associated_user_counts.first.created_at.change(:min => 0, :sec => 0)
+                  if last_history_time
+                    first_time = ap.associated_user_counts.where('created_at > ?', last_history_time).first.created_at.change(:min => 0, :sec => 0)
+                  else
+                    first_time = ap.associated_user_counts.first.created_at.change(:min => 0, :sec => 0)
+                  end
+                  last_time = ap.associated_user_counts.last.created_at.change(:min => 0, :sec => 0)
+
+                  max = ap.associated_user_counts.where(:created_at => first_time..last_time).maximum(:count)
+                  if max
+                    history = ap.associated_user_count_histories.build(:count => max, :start_time => first_time, :last_time => last_time)
+                    history.save!
+                  end
+
+                  ap.associated_user_counts.not_recent.destroy_all
                 end
-                last_time = ap.associated_user_counts.last.created_at.change(:min => 0, :sec => 0)
-
-                max = ap.associated_user_counts.where(:created_at => first_time..last_time).maximum(:count)
-                if max
-                  history = ap.associated_user_count_histories.build(:count => max, :start_time => first_time, :last_time => last_time)
-                  history.save!
-                end
-
-                ap.associated_user_counts.not_recent.destroy_all
-              end
+              }
             rescue Exception => e
               puts "[#{Time.now}] Problem in consolidate_associated_user_counts_monitoring() for wisp '#{wisp.name}', access point '#{ap.hostname}': #{e}"
               next
