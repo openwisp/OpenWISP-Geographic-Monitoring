5612dcdb97389010e2f689e300d233f36ddb275f
nemesisdesign <federico.capoano@teletu.it>
2014-07-29 15:09:14 +0200

    AP detail: map clustering disabled at high zoom (18)

--- public/javascripts/owgm.gmaps.js
+++ public/javascripts/owgm.gmaps.js
@@ -231,7 +231,8 @@ var gmaps = {
             gmaps.tmpMarker.setMap(null);
             
             gmaps.markerCluster = new MarkerClusterer(gmaps.map, gmaps.accessPoints, {
-                gridSize: 30
+                gridSize: 30,
+                maxZoom: 18
             });
         });
     }
