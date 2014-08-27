580b7f4dc2c6f99d20faccfa8c1905ca68e826e1
nemesisdesign <federico.capoano@teletu.it>
2014-08-27 15:55:27 +0200

    Customizable subject in alert emails

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,21 +1,29 @@
 class AlertMailer < ActionMailer::Base
   default :from => CONFIG['from_email']
-  
+
   add_template_helper(ApplicationHelper)
-  
+
   def notification(alert, ap, email_address, admin=true)
     @alert = alert
     @ap = ap
     @admin = admin
-    
+
     if @alert.action == 'down'
-      subject_text = I18n.t(:hostname_is_no_longer_reachable, :hostname => @ap.hostname)
+      if CONFIG['alert_down_subject_suffix'].nil?
+        subject_text = I18n.t(:hostname_is_no_longer_reachable, :hostname => @ap.hostname)
+      else
+        subject_text = CONFIG['alert_down_subject_suffix'] % { :hostname => @ap.hostname }
+      end
     elsif @alert.action. == 'up'
-      subject_text = I18n.t(:hostname_is_now_reachable_again, :hostname => @ap.hostname)
+      if CONFIG['alert_up_subject_suffix'].nil?
+        subject_text = I18n.t(:hostname_is_now_reachable_again, :hostname => @ap.hostname)
+      else
+        subject_text = CONFIG['alert_up_subject_suffix'] % { :hostname => @ap.hostname }
+      end
     end
-    
+
     subject = "#{CONFIG['mail_subject_prefix']} #{subject_text}"
-    
+
     # send mail
     mail(:to => email_address, :subject => subject)
   end
--- test/functional/alert_mailer_test.rb
+++ test/functional/alert_mailer_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class AlertMailerTest < ActionMailer::TestCase
-  # replace this with your real tests
   test "notification" do
-    
+    I18n.default_locale = :en
+
     # enable alerts for AP and ensure no alerts are present in DB
     ap = AccessPoint.with_properties_and_group.find(1)
     ap.group.alerts = true
@@ -12,17 +12,61 @@ class AlertMailerTest < ActionMailer::TestCase
     ap.group.alerts_threshold_up = 0
     ap.group.save!
     assert_equal 0, Alert.count
-    
-    ap.manager_email = 'manager@test.com'
-    
+
+    ap.properties.manager_email = 'manager@test.com'
+    ap.properties.save!
+
     # create alert and send
     ap.unreachable!
     assert_equal 1, Alert.count
-    
-    alert = Alert.last
-    alert.send_email()
-    
+
+    Alert.send_all
+
     assert !ActionMailer::Base.deliveries.empty?
-    assert ActionMailer::Base.deliveries.length, 2
+    assert_equal 2, ActionMailer::Base.deliveries.length
+
+    assert ActionMailer::Base.deliveries[0].subject.include?('wherecamp is no longer reachable')
+
+    # send up mail
+    ap.properties.manager_email = nil
+    ap.properties.save!
+    ap.reachable!
+
+    Alert.send_all
+
+    assert_equal 3, ActionMailer::Base.deliveries.length
+    assert ActionMailer::Base.deliveries[2].subject.include?('wherecamp is now reachable again')
+  end
+
+  test "notification_customized" do
+    I18n.default_locale = :en
+
+    # enable alerts for AP and ensure no alerts are present in DB
+    ap = AccessPoint.with_properties_and_group.find(1)
+    ap.group.alerts = true
+    ap.group.alerts_email = 'group@test.com'
+    ap.group.alerts_threshold_down = 0
+    ap.group.alerts_threshold_up = 0
+    ap.group.save!
+
+    CONFIG['alert_down_subject_suffix'] = "%{hostname} is DOWN"
+
+    ap.unreachable!
+    Alert.send_all
+
+    CONFIG['alert_down_subject_suffix'] = nil
+
+    assert_equal 1, ActionMailer::Base.deliveries.length
+    assert ActionMailer::Base.deliveries[0].subject.include?('wherecamp is DOWN')
+
+    CONFIG['alert_up_subject_suffix'] = "%{hostname} is UP"
+
+    ap.reachable!
+    Alert.send_all
+
+    CONFIG['alert_up_subject_suffix'] = nil
+
+    assert_equal 2, ActionMailer::Base.deliveries.length
+    assert ActionMailer::Base.deliveries[1].subject.include?('wherecamp is UP')
   end
 end
