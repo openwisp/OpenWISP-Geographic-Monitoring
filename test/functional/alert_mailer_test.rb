b6d799fdd5b200e2c51e3179cf9ad12ddef4cbd0
nemesisdesign <federico.capoano@teletu.it>
2014-08-27 18:53:50 +0200

    Customizable body in alert emails

--- test/functional/alert_mailer_test.rb
+++ test/functional/alert_mailer_test.rb
@@ -26,6 +26,8 @@ class AlertMailerTest < ActionMailer::TestCase
     assert_equal 2, ActionMailer::Base.deliveries.length
 
     assert ActionMailer::Base.deliveries[0].subject.include?('wherecamp is no longer reachable')
+    assert ActionMailer::Base.deliveries[0].body.include?('Notification type: PROBLEM')
+    assert ActionMailer::Base.deliveries[0].body.include?('URL:')
 
     # send up mail
     ap.properties.manager_email = nil
@@ -36,6 +38,7 @@ class AlertMailerTest < ActionMailer::TestCase
 
     assert_equal 3, ActionMailer::Base.deliveries.length
     assert ActionMailer::Base.deliveries[2].subject.include?('wherecamp is now reachable again')
+    assert ActionMailer::Base.deliveries[2].body.include?('Notification type: RECOVERY')
   end
 
   test "notification_customized" do
@@ -48,25 +51,69 @@ class AlertMailerTest < ActionMailer::TestCase
     ap.group.alerts_threshold_down = 0
     ap.group.alerts_threshold_up = 0
     ap.group.save!
+    ap.properties.manager_email = 'manager@test.com'
+    ap.properties.save!
 
     CONFIG['alert_down_subject_suffix'] = "%{hostname} is DOWN"
+    CONFIG['alert_up_subject_suffix'] = "%{hostname} is UP"
+    CONFIG['alert_body_text_admin'] = """Custom body text admin
+***** OpenWISP Geographic Monitoring *****
+
+Notification type: %{notification_type}
+Host: %{hostname}
+Status: %{status} since %{status_changed_at}
+
+City: %{city}
+Address: %{address}
+Ip address: %{ip}
+Mac address: %{mac_address}
+
+Notes:
+%{notes}
+
+URL: %{url}"""
+    CONFIG['alert_body_text_manager'] = """Custom body text manager
+***** OpenWISP Geographic Monitoring *****
+
+Notification type: %{notification_type}
+Host: %{hostname}
+Status: %{status} since %{status_changed_at}
+
+City: %{city}
+Address: %{address}
+Ip address: %{ip}
+Mac address: %{mac_address}
+
+Notes:
+%{notes}
+"""
 
     ap.unreachable!
     Alert.send_all
 
-    CONFIG['alert_down_subject_suffix'] = nil
-
-    assert_equal 1, ActionMailer::Base.deliveries.length
+    assert_equal 2, ActionMailer::Base.deliveries.length
+    # admin email
     assert ActionMailer::Base.deliveries[0].subject.include?('wherecamp is DOWN')
-
-    CONFIG['alert_up_subject_suffix'] = "%{hostname} is UP"
+    assert ActionMailer::Base.deliveries[0].body.include?('Notification type: PROBLEM')
+    assert ActionMailer::Base.deliveries[0].body.include?('Custom body text admin')
+    assert ActionMailer::Base.deliveries[0].body.include?('URL:')
+    # manager email
+    assert ActionMailer::Base.deliveries[1].subject.include?('wherecamp is DOWN')
+    assert ActionMailer::Base.deliveries[1].body.include?('Notification type: PROBLEM')
+    assert ActionMailer::Base.deliveries[1].body.include?('Custom body text manager')
+    assert !ActionMailer::Base.deliveries[1].body.include?('URL:')
 
     ap.reachable!
     Alert.send_all
 
-    CONFIG['alert_up_subject_suffix'] = nil
+    assert_equal 4, ActionMailer::Base.deliveries.length
+    assert ActionMailer::Base.deliveries[2].subject.include?('wherecamp is UP')
+    assert ActionMailer::Base.deliveries[2].body.include?('Notification type: RECOVERY')
+    assert ActionMailer::Base.deliveries[2].body.include?('Custom body text admin')
 
-    assert_equal 2, ActionMailer::Base.deliveries.length
-    assert ActionMailer::Base.deliveries[1].subject.include?('wherecamp is UP')
+    CONFIG['alert_down_subject_suffix'] = nil
+    CONFIG['alert_up_subject_suffix'] = nil
+    CONFIG['alert_body_text_admin'] = nil
+    CONFIG['alert_body_text_manager'] = nil
   end
 end
